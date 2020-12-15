93787e3d6c190f776721231bdd957e0e903fc885
Mangesh <mangeshs@bsf.io>
2020-12-15 11:02:46 +0530

    Removed space

--- dist/blocks-deactivate.js
+++ dist/blocks-deactivate.js
@@ -1,6 +1,5 @@
 let uagb_deactivated_blocks = uagb_deactivate_blocks.deactivated_blocks
 // If we are recieving an object, let's convert it into an array.
-
 if ( uagb_deactivated_blocks.length ) {
 	if ( typeof wp.blocks.unregisterBlockType !== "undefined" ) {
 		for( block_index in uagb_deactivated_blocks ) {
