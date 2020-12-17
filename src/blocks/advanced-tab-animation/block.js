12fd143130efed160579019e7ceefd36ec363d78
Mangesh <mangeshs@bsf.io>
2020-12-17 17:12:21 +0530

    Fix - Price List issue for animation

--- src/blocks/advanced-tab-animation/block.js
+++ src/blocks/advanced-tab-animation/block.js
@@ -11,7 +11,7 @@ const AdvancedAnimationControls = wp.compose.createHigherOrderComponent((BlockEd
 		const { InspectorAdvancedControls } = wp.blockEditor;
 		const { attributes, setAttributes, isSelected } = props;
 		const blocks_name = props.name;
-		const block_type = ['uagb/buttons-child','uagb/faq-child', 'uagb/icon-list-child', 'uagb/social-share-child' ];
+		const block_type = ['uagb/buttons-child','uagb/faq-child', 'uagb/icon-list-child', 'uagb/social-share-child','uagb/restaurant-menu-child' ];
 
 		return (
 			<Fragment>
@@ -24,23 +24,23 @@ const AdvancedAnimationControls = wp.compose.createHigherOrderComponent((BlockEd
 							onChange={ ( value ) => setAttributes( { animationfield: value } ) }
 							options={ [
 								{ value: "none", label: __( "None" ) },
-								{ value: "fadeInDown", label: __( "fadeInDown" ) },
-								{ value: "fadeInUp", label: __( "fadeInUp" ) },
-								{ value: "fadeIn", label: __( "fadeIn" ) },
-								{ value: "fadeInLeft", label: __( "fadeInLeft" ) },
-								{ value: "fadeInRight", label: __( "fadeInRight" ) },
-								{ value: "lightSpeedIn", label: __( "lightSpeedIn" ) },
-								{ value: "slideInDown", label: __( "slideInDown" ) },
-								{ value: "slideInLeft", label: __( "slideInLeft" ) },
-								{ value: "slideInRight", label: __( "slideInRight" ) },
-								{ value: "slideInUp", label: __( "slideInUp" ) },
-								{ value: "bounceIn", label: __( "bounceIn" ) },
-								{ value: "bounceDown", label: __( "bounceDown" ) },
-								{ value: "bounceLeft", label: __( "bounceLeft" ) },
-								{ value: "bounceInRight", label: __( "bounceInRight" ) },
-								{ value: "bounceInUp", label: __( "bounceInUp" ) },
-								{ value: "rotateIn", label: __( "rotateIn" ) },
-								{ value: "rotateInDownLeft", label: __( "rotateInDownLeft" ) },							
+								{ value: "fadeInDown", label: __( "Fade In Down" ) },
+								{ value: "fadeInUp", label: __( "Fade In Up" ) },
+								{ value: "fadeIn", label: __( "Fade In" ) },
+								{ value: "fadeInLeft", label: __( "Fade In Left" ) },
+								{ value: "fadeInRight", label: __( "Fade In Right" ) },
+								{ value: "lightSpeedIn", label: __( "Light Speed In" ) },
+								{ value: "slideInDown", label: __( "Slide In Down" ) },
+								{ value: "slideInLeft", label: __( "Slide In Left" ) },
+								{ value: "slideInRight", label: __( "Slide In Right" ) },
+								{ value: "slideInUp", label: __( "Slide In Up" ) },
+								{ value: "bounceIn", label: __( "Bounce In" ) },
+								{ value: "bounceDown", label: __( "Bounce Down" ) },
+								{ value: "bounceLeft", label: __( "Bounce Left" ) },
+								{ value: "bounceInRight", label: __( "Bounce In Right" ) },
+								{ value: "bounceInUp", label: __( "Bounce In Up" ) },
+								{ value: "rotateIn", label: __( "Rotate In" ) },
+								{ value: "rotateInDownLeft", label: __( "Rotate In Down Left" ) },
 							] }
 							help={ __( "This settings will only take effect once you are on the live page, and not while you're editing in Gutenberg." ) } 
 						/>
@@ -55,8 +55,8 @@ const AdvancedAnimationControls = wp.compose.createHigherOrderComponent((BlockEd
 								{ value: "bounce", label: __( "Bounce" ) },
 								{ value: "pop", label: __( "Pop" ) },
 								{ value: "grow", label: __( "Grow" ) },
-								{ value: "pulse-grow", label: __( "Pulse-grow" ) },
-								{ value: "pulse-shrink", label: __( "Pulse-shrink" ) },							
+								{ value: "pulse-grow", label: __( "Pulse Grow" ) },
+								{ value: "pulse-shrink", label: __( "Pulse Shrink" ) },
 							] }
 							help={ __( "This settings will only take effect once you are on the live page, and not while you're editing in Gutenberg." ) }
 						/>
@@ -78,7 +78,7 @@ function ApplyAnimateClass(extraProps, blockType, attributes) {
 	const { 
 		animationfield,
 		animationhoverfield
-	 } = attributes;
+	} = attributes;
 	
 	if ( animationfield && 'none' !== animationfield) {	
 		extraProps.className = extraProps.className + ' uag-animation-' + animationfield;
