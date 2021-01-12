c35d2459233a20001768bce6f86379f44b1691ea
Mangesh <mangeshs@bsf.io>
2021-01-12 12:19:00 +0530

    Added text-domain

--- src/blocks/advanced-tab-animation/block.js
+++ src/blocks/advanced-tab-animation/block.js
@@ -19,46 +19,46 @@ const AdvancedAnimationControls = wp.compose.createHigherOrderComponent((BlockEd
 				{isSelected &&  ! block_type.includes(blocks_name) &&
 					<InspectorAdvancedControls>
 						<span className="components-base-control__help">
-							{ __( "This settings will only take effect once you are on the live page, and not while you're editing in Gutenberg." ) }
+							{ __( "This settings will only take effect once you are on the live page, and not while you're editing in Gutenberg.", 'ultimate-addons-for-gutenberg' ) }
 						</span>
 						<SelectControl
-							label={ __( "Animation" ) }
+							label={ __( "Animation", 'ultimate-addons-for-gutenberg' ) }
 							value={ attributes.animationfield }
 							onChange={ ( value ) => setAttributes( { animationfield: value } ) }
 							options={ [
-								{ value: "none", label: __( "None" ) },
-								{ value: "fadeInDown", label: __( "Fade In Down" ) },
-								{ value: "fadeInUp", label: __( "Fade In Up" ) },
-								{ value: "fadeIn", label: __( "Fade In" ) },
-								{ value: "fadeInLeft", label: __( "Fade In Left" ) },
-								{ value: "fadeInRight", label: __( "Fade In Right" ) },
-								{ value: "lightSpeedIn", label: __( "Light Speed In" ) },
-								{ value: "slideInDown", label: __( "Slide In Down" ) },
-								{ value: "slideInLeft", label: __( "Slide In Left" ) },
-								{ value: "slideInRight", label: __( "Slide In Right" ) },
-								{ value: "slideInUp", label: __( "Slide In Up" ) },
-								{ value: "bounceIn", label: __( "Bounce In" ) },
-								{ value: "bounceDown", label: __( "Bounce Down" ) },
-								{ value: "bounceLeft", label: __( "Bounce Left" ) },
-								{ value: "bounceInRight", label: __( "Bounce In Right" ) },
-								{ value: "bounceInUp", label: __( "Bounce In Up" ) },
-								{ value: "rotateIn", label: __( "Rotate In" ) },
-								{ value: "rotateInDownLeft", label: __( "Rotate In Down Left" ) },
+								{ value: "none", label: __( "None", 'ultimate-addons-for-gutenberg' ) },
+								{ value: "fadeInDown", label: __( "Fade In Down", 'ultimate-addons-for-gutenberg' ) },
+								{ value: "fadeInUp", label: __( "Fade In Up", 'ultimate-addons-for-gutenberg' ) },
+								{ value: "fadeIn", label: __( "Fade In", 'ultimate-addons-for-gutenberg' ) },
+								{ value: "fadeInLeft", label: __( "Fade In Left", 'ultimate-addons-for-gutenberg' ) },
+								{ value: "fadeInRight", label: __( "Fade In Right", 'ultimate-addons-for-gutenberg' ) },
+								{ value: "lightSpeedIn", label: __( "Light Speed In", 'ultimate-addons-for-gutenberg' ) },
+								{ value: "slideInDown", label: __( "Slide In Down", 'ultimate-addons-for-gutenberg' ) },
+								{ value: "slideInLeft", label: __( "Slide In Left", 'ultimate-addons-for-gutenberg' ) },
+								{ value: "slideInRight", label: __( "Slide In Right", 'ultimate-addons-for-gutenberg' ) },
+								{ value: "slideInUp", label: __( "Slide In Up", 'ultimate-addons-for-gutenberg' ) },
+								{ value: "bounceIn", label: __( "Bounce In", 'ultimate-addons-for-gutenberg' ) },
+								{ value: "bounceDown", label: __( "Bounce Down", 'ultimate-addons-for-gutenberg' ) },
+								{ value: "bounceLeft", label: __( "Bounce Left", 'ultimate-addons-for-gutenberg' ) },
+								{ value: "bounceInRight", label: __( "Bounce In Right", 'ultimate-addons-for-gutenberg' ) },
+								{ value: "bounceInUp", label: __( "Bounce In Up", 'ultimate-addons-for-gutenberg' ) },
+								{ value: "rotateIn", label: __( "Rotate In", 'ultimate-addons-for-gutenberg' ) },
+								{ value: "rotateInDownLeft", label: __( "Rotate In Down Left", 'ultimate-addons-for-gutenberg' ) },
 							] } 
 						/>
 						<SelectControl
-							label={ __( "Hover Animation" ) }
+							label={ __( "Hover Animation", 'ultimate-addons-for-gutenberg' ) }
 							value={ attributes.animationhoverfield }
 							onChange={ ( value ) => setAttributes( { animationhoverfield: value } ) }
 							options={ [
-								{ value: "none", label: __( "None" ) },
-								{ value: "push", label: __( "Push" ) },
-								{ value: "pulse", label: __( "Pulse" ) },
-								{ value: "bounce", label: __( "Bounce" ) },
-								{ value: "pop", label: __( "Pop" ) },
-								{ value: "grow", label: __( "Grow" ) },
-								{ value: "pulse-grow", label: __( "Pulse Grow" ) },
-								{ value: "pulse-shrink", label: __( "Pulse Shrink" ) },
+								{ value: "none", label: __( "None", 'ultimate-addons-for-gutenberg' ) },
+								{ value: "push", label: __( "Push", 'ultimate-addons-for-gutenberg' ) },
+								{ value: "pulse", label: __( "Pulse", 'ultimate-addons-for-gutenberg' ) },
+								{ value: "bounce", label: __( "Bounce", 'ultimate-addons-for-gutenberg' ) },
+								{ value: "pop", label: __( "Pop", 'ultimate-addons-for-gutenberg' ) },
+								{ value: "grow", label: __( "Grow", 'ultimate-addons-for-gutenberg' ) },
+								{ value: "pulse-grow", label: __( "Pulse Grow", 'ultimate-addons-for-gutenberg' ) },
+								{ value: "pulse-shrink", label: __( "Pulse Shrink", 'ultimate-addons-for-gutenberg' ) },
 							] }
 						/>
 					</InspectorAdvancedControls>
