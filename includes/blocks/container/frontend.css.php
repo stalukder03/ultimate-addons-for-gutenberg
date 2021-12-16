5b3b9a3c00b016f357dab2715202ea4dad2a7b2c
RajkiranBagal <rajkiranb@bsf.io>
2021-12-16 10:33:31 +0530

    Added frontend BG CSS

--- includes/blocks/container/frontend.css.php
+++ includes/blocks/container/frontend.css.php
@@ -6,17 +6,32 @@
  *
  * @package uagb
  */
+$bg_obj = array(
+	'backgroundType' => $attr['backgroundType'],
+	'backgroundImage' => $attr['backgroundImage'],
+	'backgroundColor' => $attr['backgroundColor'],
+	'gradientValue' => $attr['gradientValue'],
+	'backgroundRepeat' => $attr['backgroundRepeat'],
+	'backgroundPosition' => $attr['backgroundPosition'],
+	'backgroundSize' => $attr['backgroundSize'],
+	'backgroundAttachment' => $attr['backgroundAttachment'],
+);
+
+$container_bg_css = UAGB_Block_Helper::uag_get_background_obj($bg_obj);
+$container_css = array(
+	'width'           => UAGB_Helper::get_css_value( $attr['widthDesktop'], $attr['widthType'] ),
+	'min-height'      => UAGB_Helper::get_css_value( $attr['minHeightDesktop'], $attr['minHeightType'] ),
+	'flex-direction'  => $attr['directionDesktop'],
+	'align-items'     => $attr['alignItemsDesktop'],
+	'justify-content' => $attr['justifyContentDesktop'],
+	'flex-wrap'       => $attr['wrapDesktop'],
+	'align-content'   => $attr['alignContentDesktop'],
+);
+
+$container_css = array_merge($container_css, $container_bg_css);
 
 $selectors = array(
-	'.wp-block-uagb-container' => array(
-		'width'           => UAGB_Helper::get_css_value( $attr['widthDesktop'], $attr['widthType'] ),
-		'min-height'      => UAGB_Helper::get_css_value( $attr['minHeightDesktop'], $attr['minHeightType'] ),
-		'flex-direction'  => $attr['directionDesktop'],
-		'align-items'     => $attr['alignItemsDesktop'],
-		'justify-content' => $attr['justifyContentDesktop'],
-		'flex-wrap'       => $attr['wrapDesktop'],
-		'align-content'   => $attr['alignContentDesktop'],
-	),
+	'.wp-block-uagb-container' => $container_css
 );
 
 $t_selectors = array(
