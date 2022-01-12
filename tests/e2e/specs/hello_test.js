e8352aebf8e5575a984a8cdae6b7b60b72a50548
akshaylakh <akshayl@bsf.io>
2022-01-12 18:08:25 +0530

    fixed quote-props errors in some files

--- tests/e2e/specs/hello_test.js
+++ tests/e2e/specs/hello_test.js
@@ -2,8 +2,8 @@ import { insertBlock, createNewPost } from '@wordpress/e2e-test-utils';
 describe( 'List in gutenberg editor', () => {
 	it( 'assert wide width of the list in the block editor', async () => {
 		await createNewPost( {
-			postType: 'post',
-			title: 'test List',
+			'postType': 'post',
+			'title': 'test List',
 		} );
 		await insertBlock( 'List' );
 		await page.keyboard.type( 'one' );
@@ -13,9 +13,9 @@ describe( 'List in gutenberg editor', () => {
 			'.editor-styles-wrapper > .block-editor-block-list__layout',
 		);
 		await expect( {
-			selector:
+			'selector':
 				'.editor-styles-wrapper > .block-editor-block-list__layout',
-			property: 'width',
+			'property': 'width',
 		} ).cssValueToBe( `1119px` );
 	} );
 } );
