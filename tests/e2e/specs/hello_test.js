3e794b9b3387e2e3e5c4b1a91fa183f0e8885f05
akshaylakh <akshayl@bsf.io>
2022-01-18 11:18:15 +0530

    updated test case for testing

--- tests/e2e/specs/hello_test.js
+++ tests/e2e/specs/hello_test.js
@@ -1,21 +1,14 @@
-import { insertBlock, createNewPost } from '@wordpress/e2e-test-utils';
-describe( 'List in gutenberg editor', () => {
-	it( 'assert wide width of the list in the block editor', async () => {
-		await createNewPost( {
-			'postType': 'post',
-			'title': 'test List',
-		} );
-		await insertBlock( 'List' );
-		await page.keyboard.type( 'one' );
-		await page.keyboard.press( 'Enter' );
-		await page.keyboard.type( 'two' );
-		await page.waitForSelector(
-			'.editor-styles-wrapper > .block-editor-block-list__layout',
-		);
-		await expect( {
-			'selector':
-				'.editor-styles-wrapper > .block-editor-block-list__layout',
-			'property': 'width',
-		} ).cssValueToBe( `1119px` );
-	} );
-} );
+/**
+ * WordPress dependencies
+ */
+ import { visitAdminPage } from '@wordpress/e2e-test-utils';
+
+ describe( 'Hello World', () => {
+	 it( 'should load properly', async () => {
+		 await visitAdminPage( '/' );
+		 const nodes = await page.$x(
+			 '//h2[contains(text(), "Welcome to WordPress!")]',
+		 );
+		 await expect( nodes ).not.toHaveLength( 0 );
+	 } );
+ } );
