2a3ea00ad19da12965fbeeb951da98b1d87ac31a
Tushar Imran <tusharimran9909@gmail.com>
2022-02-16 18:43:29 +0600

    Image Block - added editor css support for typography and space control

--- src/blocks/image/styling.js
+++ src/blocks/image/styling.js
@@ -12,6 +12,35 @@ export default function styling( props ) {
 		width,
 		height,
 		align,
+		captionFontFamily,
+		captionFontWeight,
+		captionFontStyle,
+		captionFontSize,
+		captionColor,
+		captionTransform,
+		captionDecoration,
+		captionFontSizeType,
+		captionFontSizeMobile,
+		captionFontSizeTablet,
+		captionLineHeight,
+		captionLineHeightType,
+		captionLineHeightMobile,
+		captionLineHeightTablet,
+		captionTopMargin,
+		captionRightMargin,
+		captionLeftMargin,
+		captionBottomMargin,
+		captionTopMarginTablet,
+		captionRightMarginTablet,
+		captionLeftMarginTablet,
+		captionBottomMarginTablet,
+		captionTopMarginMobile,
+		captionRightMarginMobile,
+		captionLeftMarginMobile,
+		captionBottomMarginMobile,
+		captionMarginUnit,
+		captionMarginUnitTablet,
+		captionMarginUnitMobile,
 	} = props.attributes;
 
 	const selectors = {
@@ -19,6 +48,38 @@ export default function styling( props ) {
 			'width': width || 'inherit',
 			'height': height || 'inherit',
 		},
+		'.wp-block-uagb-image figcaption': {
+			'font-family': captionFontFamily,
+			'font-style' : captionFontStyle,
+			'text-decoration': captionDecoration,
+			'text-transform': captionTransform,
+			'font-weight': captionFontWeight,
+			'font-size': generateCSSUnit(
+				captionFontSize,
+				captionFontSizeType
+			),
+			'line-height': generateCSSUnit(
+				captionLineHeight,
+				captionLineHeightType
+			),
+			'color': captionColor,
+			'margin-top': generateCSSUnit(
+				captionTopMargin,
+				captionMarginUnit
+			),
+			'margin-right': generateCSSUnit(
+				captionRightMargin,
+				captionMarginUnit
+			),
+			'margin-bottom': generateCSSUnit(
+				captionBottomMargin,
+				captionMarginUnit
+			),
+			'margin-left': generateCSSUnit(
+				captionLeftMargin,
+				captionMarginUnit
+			),
+		}
 	}
 
 	if( align === 'right' ){
@@ -32,10 +93,79 @@ export default function styling( props ) {
 		8
 	) }`;
 
-	const styling_css = generateCSS( selectors, base_selector );
-
-
 	const tablet_selectors = {};
 	const mobile_selectors = {};
+
+	tablet_selectors[' .wp-block-uagb-image figcaption'] = {
+        'font-size': generateCSSUnit(
+            captionFontSizeTablet,
+            captionFontSizeType
+        ),
+        'line-height': generateCSSUnit(
+            captionLineHeightTablet,
+            captionLineHeightType
+        ),
+		'margin-top': generateCSSUnit(
+			captionTopMarginTablet,
+			captionMarginUnitTablet
+		),
+		'margin-right': generateCSSUnit(
+			captionRightMarginTablet,
+			captionMarginUnitTablet
+		),
+		'margin-bottom': generateCSSUnit(
+			captionBottomMarginTablet,
+			captionMarginUnitTablet
+		),
+		'margin-left': generateCSSUnit(
+			captionLeftMarginTablet,
+			captionMarginUnitTablet
+		),
+    }
+
+	mobile_selectors[' .wp-block-uagb-image figcaption'] = {
+        'font-size': generateCSSUnit(
+            captionFontSizeMobile,
+            captionFontSizeType
+        ),
+        'line-height': generateCSSUnit(
+            captionLineHeightMobile,
+            captionLineHeightType
+        ),
+		'margin-top': generateCSSUnit(
+			captionTopMarginMobile,
+			captionMarginUnitMobile
+		),
+		'margin-right': generateCSSUnit(
+			captionRightMarginMobile,
+			captionMarginUnitMobile
+		),
+		'margin-bottom': generateCSSUnit(
+			captionBottomMarginMobile,
+			captionMarginUnitMobile
+		),
+		'margin-left': generateCSSUnit(
+			captionLeftMarginMobile,
+			captionMarginUnitMobile
+		),
+    }
+
+	let styling_css = generateCSS( selectors, base_selector );
+
+	styling_css += generateCSS(
+        tablet_selectors,
+        `${ base_selector }.uagb-editor-preview-mode-tablet`,
+        true,
+        'tablet'
+    );
+
+
+    styling_css += generateCSS(
+        mobile_selectors,
+        `${ base_selector }.uagb-editor-preview-mode-mobile`,
+        true,
+        'mobile'
+    );
+
 	return styling_css;
 }
