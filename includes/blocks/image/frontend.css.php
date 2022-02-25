41934b920f2bdc326beb02733eaeaa57d6443d60
Tushar Imran <tusharimran9909@gmail.com>
2022-02-25 12:27:48 +0600

    Image Block - added seperator dynamic css support

--- includes/blocks/image/frontend.css.php
+++ includes/blocks/image/frontend.css.php
@@ -10,6 +10,10 @@ $selectors = array(
 	'.wp-block-uagb-image img'   => array(
 		'width' => (isset($attr['width']) ? $attr['width'] : 'inherit'),
 		'height' => (isset($attr['height']) ? $attr['height'] : 'inherit'),
+		'border-style' => $attr['imageBorderStyle'],
+		'border-color' => $attr['imageBorderColor'],
+		'border-radius' => UAGB_Helper::get_css_value( $attr['imageBorderRadius'], 'px' ),
+		'border-width' => UAGB_Helper::get_css_value( $attr['imageBorderWidth'], 'px' ),
 	),
 	'.wp-block-uagb-image figcaption' => array(
 		'font-family' => $attr['captionFontFamily'],
@@ -25,7 +29,18 @@ $selectors = array(
 		'margin-bottom' => UAGB_Helper::get_css_value( $attr['captionBottomMargin'], $attr['captionMarginUnit'] ),
 		'margin-left' => UAGB_Helper::get_css_value( $attr['captionLeftMargin'], $attr['captionMarginUnit'] ),
 	),
+	' .wp-block-uagb-image--layout-overlay__inner .uagb-image-separator' => array(
+		'width' => UAGB_Helper::get_css_value( $attr['seperatorWidth'], $attr['separatorWidthType'] ),
+		'border-top-width' => UAGB_Helper::get_css_value( $attr['seperatorThickness'], $attr['seperatorThicknessUnit'] ),
+		'border-top-color' => $attr['seperatorColor'],
+		'border-top-style' => $attr['seperatorStyle'],
+		'margin-bottom' => UAGB_Helper::get_css_value( $attr['seperatorBottomMargin'], $attr['seperatorMarginUnit'] ),
+		'margin-top' => UAGB_Helper::get_css_value( $attr['seperatorTopMargin'], $attr['seperatorMarginUnit'] ),
+		'margin-left' => UAGB_Helper::get_css_value( $attr['seperatorLeftMargin'], $attr['seperatorMarginUnit'] ),
+		'margin-right' => UAGB_Helper::get_css_value( $attr['seperatorRightMargin'], $attr['seperatorMarginUnit'] ),
+	)
 );
+
 
 if($attr['maskShape'] !== 'none'){
 	$imagePath = UAGB_URL . 'assets/images/masks/' . $attr['maskShape']  . '.svg';
