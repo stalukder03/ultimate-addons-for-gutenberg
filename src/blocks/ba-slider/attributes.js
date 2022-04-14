75c31f348b80979e953c4e74a57b96129cc65569
Antariksh Patre <antarikshpatre@gmail.com>
2022-04-14 14:50:50 +0530

    Added block_id and classMigrate attributes

--- src/blocks/ba-slider/attributes.js
+++ src/blocks/ba-slider/attributes.js
@@ -1,7 +1,13 @@
 import { __ } from '@wordpress/i18n';
 
 const attributes = {
-    
+    block_id: {
+		type: 'string',
+	},
+	classMigrate: {
+		type: 'boolean',
+		default: false,
+	},
 };
 
 export default attributes;
