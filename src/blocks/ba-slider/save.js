ed21ac373421cbb5dbe5271790edd90b7d37bd0b
Antariksh Patre <antarikshpatre@gmail.com>
2022-04-14 15:53:54 +0530

    Created basic save.js file

---
+++ src/blocks/ba-slider/save.js
@@ -0,0 +1,12 @@
+/**
+ * BLOCK: Image Comparison Slider / Before-After Slider
+ */
+
+// Import block dependencies and components.
+import classnames from 'classnames';
+import { RichText } from '@wordpress/block-editor';
+import { __ } from '@wordpress/i18n';
+
+export default function save( props ) {
+	return null;  // Server side rendering (tentative).
+}
