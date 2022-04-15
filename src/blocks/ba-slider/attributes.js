9b0ea0e938ac5ac46562a021c16d911f0177f4e3
Antariksh Patre <antarikshpatre@gmail.com>
2022-04-15 14:53:16 +0530

    Registered new attributes based on General Settings tab requirements

--- src/blocks/ba-slider/attributes.js
+++ src/blocks/ba-slider/attributes.js
@@ -9,12 +9,54 @@ const attributes = {
 		type: 'boolean',
 		default: false,
 	},
+	
+	// <-- GENERAL SETTINGS -->
+
+	// Images.
 	beforeImage: {
 		type: 'object',
 	},
 	afterImage: {
 		type: 'object',
 	},
+
+	// Image Labels.
+	showLabels: {
+		type: 'boolean',
+		default: true,
+	},
+	beforeLabel: {
+		type: 'string',
+		default: __( 'Before' , 'ultimate-addons-for-gutenberg' ),
+	},
+	afterLabel: {
+		type: 'string',
+		default: __( 'After' , 'ultimate-addons-for-gutenberg' ),
+	},
+
+	// Slider Orientation ( 0 = Horizontal, 1 = Vertical ).
+	sliderOrientation: {
+		type: 'number',
+		default: 0,	// Horizontal orientation by default.
+	},
+
+	// Slider position in percentage.
+	sliderPosition: {
+		type: 'number',
+		default: 50,
+	},
+
+	animateSlider: {
+		type: 'boolean',
+		default: false,	// Disable animated slider by default.
+	},
+
+	// Slide handle on mouse hover.
+	hoverSlider: {
+		type: 'boolean',
+		default: false,
+	},
+
 };
 
 export default attributes;
