a8905bd2de3c03a42b129ae7fb6b202169ce4344
Antariksh Patre <antarikshpatre@gmail.com>
2022-04-19 13:53:29 +0530

    Created basic include files

---
+++ includes/blocks/ba-slider/block.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Block Information & Attributes File.
+ *
+ * @since 2.0.0
+ *
+ * @package uagb
+ */
+
+$block_slug = 'uagb/ba-slider';
+$block_data = array(
+	'slug'             => '',
+	'admin_categories' => array( 'content' ),
+	'link'             => 'ba-slider',
+	'doc'              => 'ba-slider',
+	'title'            => __( 'Image Comparison Block', 'ultimate-addons-for-gutenberg' ),
+	'description'      => __( 'This block allows visitors to compare two images using a slider.', 'ultimate-addons-for-gutenberg' ),
+	'default'          => true,
+	'extension'        => false,
+	'priority'         => 2,
+);
---
+++ includes/blocks/ba-slider/frontend.css.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Frontend CSS & Google Fonts loading File.
+ *
+ * @since 2.0.0
+ *
+ * @package uagb
+ */
