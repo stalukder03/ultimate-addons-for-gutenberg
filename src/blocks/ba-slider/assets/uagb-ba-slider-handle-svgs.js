3c77748a5ca6b7500ca6176ef372c4fbc9947131
Antariksh Patre <antarikshpatre@gmail.com>
2022-04-22 15:23:08 +0530

    Created SVG icon 'store' file

---
+++ src/blocks/ba-slider/assets/uagb-ba-slider-handle-svgs.js
@@ -0,0 +1,8 @@
+// Please insert slot="handle" attribute for each new icon.
+
+export default {
+    default: `<svg slot="handle" xmlns="http://www.w3.org/2000/svg" class="default-handle" viewBox="-8 -3 16 6"> <path d="M -5 -2 L -7 0 L -5 2 M 5 -2 L 7 0 L 5 2" fill="none" vector-effect="non-scaling-stroke"></path> </svg>`,
+    arrows1: `<svg slot="handle" xmlns="http://www.w3.org/2000/svg" width="100" viewBox="-8 -3 16 6"><path stroke="#fff" d="M -5 -2 L -7 0 L -5 2 M -5 -2 L -5 2 M 5 -2 L 7 0 L 5 2 M 5 -2 L 5 2" stroke-width="1" fill="#fff" vector-effect="non-scaling-stroke"></path></svg>`,
+    circle: `<svg slot="handle" xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50"><circle id="Ellipse_1" data-name="Ellipse 1" cx="25" cy="25" r="25" fill="#fff"/></svg>`,
+    square: `<svg slot="handle" xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50"><rect id="Rectangle_1" data-name="Rectangle 1" width="50" height="50" fill="#fff"/></svg>`,
+}
