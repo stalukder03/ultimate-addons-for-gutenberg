a5b551a4403385a41211f8fb02a5b4b2cc360995
Antariksh Patre <antarikshpatre@gmail.com>
2022-04-29 15:24:02 +0530

    Removed unused imports and vars in settings.js

--- src/blocks/ba-slider/settings.js
+++ src/blocks/ba-slider/settings.js
@@ -1,26 +1,19 @@
-import React, { Suspense } from 'react';
-import lazyLoader from '@Controls/lazy-loader';
-import TypographyControl from '@Components/typography';
-import WebfontLoader from '@Components/typography/fontloader';
+import React from 'react';
 import { __ } from '@wordpress/i18n';
-import { select } from '@wordpress/data';
 import Range from '@Components/range/Range.js';
 import ResponsiveSlider from '@Components/responsive-slider';
 import ResponsiveSelectControl from '@Components/responsive-select';
 import AdvancedPopColorControl from '@Components/color-control/advanced-pop-color-control.js';
-import SpacingControl from '@Components/spacing-control';
 import MultiButtonsControl from '@Components/multi-buttons-control';
 import InspectorTabs from '@Components/inspector-tabs/InspectorTabs.js';
 import InspectorTab, {
 	UAGTabs,
 } from '@Components/inspector-tabs/InspectorTab.js';
-import renderSVG from '@Controls/renderIcon';
-import UAGPresets from '@Components/presets';
 import UAGTabsControl from '@Components/tabs'
 
 import { InspectorControls } from '@wordpress/block-editor';
 
-import { SelectControl, RangeControl, Icon, ToggleControl, TextControl, __experimentalNumberControl as NumberControl } from '@wordpress/components';
+import { ToggleControl, TextControl } from '@wordpress/components';
 
 import Border from '@Components/border/index.js';
 import BoxShadowControl from '@Components/box-shadow/index.js';
@@ -28,15 +21,10 @@ import BoxShadowControl from '@Components/box-shadow/index.js';
 // Extend component
 import UAGAdvancedPanelBody from '@Components/advanced-panel-body';
 import UAGImage from '@Components/image';
-import ResponsiveToggle from '@Components/responsive-toggle';
-
-import { useDeviceType } from '@Controls/getPreviewType';
 
 const Settings = ( props ) => {
 	props = props.parentProps;
 	const { attributes, setAttributes } = props;
-
-	const deviceType = useDeviceType();
 
     /*
 	 * Event to set Image as null while removing.
