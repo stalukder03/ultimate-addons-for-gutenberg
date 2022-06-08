37153db5b8635f99f7fe261cd08c02f4fdda5b5e
AbhijitNage123 <abhiitn@bsf.io>
2022-06-08 13:34:40 +0530

    Resolved widget styling not applying on frontend

--- classes/class-uagb-post-assets.php
+++ classes/class-uagb-post-assets.php
@@ -172,7 +172,29 @@ class UAGB_Post_Assets {
 			global $post;
 			$this_post = $this->preview ? $post : get_post( $this->post_id );
 			$this->prepare_assets( $this_post );
+			$content = get_option( 'widget_block' );
+			$this->prepare_widget_area_assets( $content );
 		}
+	}
+
+	/**
+	 * Generates stylesheet for widget area.
+	 *
+	 * @param object $content Current Post Object.
+	 * @since x.x.x
+	 */
+	public function prepare_widget_area_assets( $content ) {
+
+		if ( empty( $content ) ) {
+			return;
+		}
+
+		foreach ( $content as $key => $value ) {
+			if ( is_array( $value ) && isset( $value['content'] ) && has_blocks( $value['content'] ) ) {
+				$this->common_function_for_assets_preparation( $value['content'] );
+			}
+		}
+
 	}
 
 	/**
@@ -301,6 +323,33 @@ class UAGB_Post_Assets {
 				add_action( 'wp_footer', array( $this, 'print_script' ), 1000 );
 			}
 		}
+
+		if ( has_blocks( $this_post->ID ) && isset( $this_post->post_content ) ) {
+			$this->common_function_for_assets_preparation( $this_post->post_content );
+		}
+	}
+
+	/**
+	 * Common function to generate stylesheet.
+	 *
+	 * @param array $post_content Current Post Object.
+	 * @since x.x.x
+	 */
+	public function common_function_for_assets_preparation( $post_content ) {
+		$blocks            = $this->parse_blocks( $post_content );
+		$this->page_blocks = $blocks;
+
+		if ( ! is_array( $blocks ) || empty( $blocks ) ) {
+			return;
+		}
+
+		$assets = $this->get_blocks_assets( $blocks );
+
+		$this->stylesheet .= $assets['css'];
+		$this->script     .= $assets['js'];
+
+		// Update fonts.
+		$this->gfonts = array_merge( $this->gfonts, UAGB_Helper::$gfonts );
 	}
 
 
