d9c73fe4c01940f787df8ca6e48050bf04e73e68
Vikas Patil <patilvikasj@gmail.com>
2022-10-12 15:25:52 +0530

    Fix: block inserter not visible for slide child block

--- src/blocks/slider-child/edit.js
+++ src/blocks/slider-child/edit.js
@@ -16,7 +16,8 @@ import Render from './render';
 import './style.scss';
 import { __ } from '@wordpress/i18n';
 
-import { select } from '@wordpress/data';
+import { withSelect, useDispatch, select } from '@wordpress/data';
+import { compose } from '@wordpress/compose';
 
 import {
 	__experimentalBlockVariationPicker as BlockVariationPicker,
@@ -175,6 +176,10 @@ const UAGBSlide = ( props ) => {
 
 	const previewImageData = `${ uagb_blocks_info.uagb_url }/admin/assets/preview-images/container.png`;
 
+	console.log( variationSelected );
+	console.log( select( 'core/block-editor' ).getBlockParents( props.clientId ).length );
+
+
 	if ( ! variationSelected && 0 === select( 'core/block-editor' ).getBlockParents( props.clientId ).length ) {
 
 		return (
@@ -210,5 +215,37 @@ const UAGBSlide = ( props ) => {
 	);
 };
 
+const applyWithSelect = withSelect( ( select, props ) => { // eslint-disable-line no-shadow
+	const { __experimentalGetPreviewDeviceType = null } = select(
+		'core/edit-post'
+	);
+	const deviceType = __experimentalGetPreviewDeviceType
+		? __experimentalGetPreviewDeviceType()
+		: null;
+		const { getBlocks } = select( 'core/block-editor' );
+	const {
+		getBlockType,
+		getBlockVariations,
+		getDefaultBlockVariation,
+	} = select( 'core/blocks' );
+	const innerBlocks = getBlocks( props.clientId );
+	const { replaceInnerBlocks } = useDispatch( 'core/block-editor' );
 
-export default UAGBSlide;
+	return {
+		// Subscribe to changes of the innerBlocks to control the display of the layout selection placeholder.
+		innerBlocks,
+		blockType: getBlockType( props.name ),
+		defaultVariation:
+			typeof getDefaultBlockVariation === 'undefined'
+				? null
+				: getDefaultBlockVariation( props.name ),
+		variations:
+			typeof getBlockVariations === 'undefined'
+				? null
+				: getBlockVariations( props.name ),
+		replaceInnerBlocks,
+		deviceType,
+		isParentOfSelectedBlock: select( 'core/block-editor' ).hasSelectedInnerBlock( props.clientId, true )
+	};
+} );
+export default compose( applyWithSelect )( UAGBSlide );
