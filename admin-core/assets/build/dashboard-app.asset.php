d975f2fa27ed0b416a8fc30947e09ef7f44e8599
Zane Vaz <zanev@bsf.io>
2022-10-12 10:30:51 +0530

    SPCTR-556: Updated Block Icons

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-i18n'), 'version' => '81e71264656ae2d5056345ea1ef66044');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-i18n'), 'version' => 'f31f81c60382a2e9ae3841d643afbbe7');
