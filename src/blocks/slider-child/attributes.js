b49210b01e11cf6fd90935240ffe0680deabe477
Vikas Patil <patilvikasj@gmail.com>
2022-10-31 11:08:23 +0530

    Remove unwanted attributes

--- src/blocks/slider-child/attributes.js
+++ src/blocks/slider-child/attributes.js
@@ -6,14 +6,6 @@ const attributes = {
 	block_id: {
 		type: 'string',
 	},
-	// Container wrapper tag name and link are not required in PHP as it is only used in render and save.
-	htmlTag: {
-		type: 'string',
-		default: 'div',
-	},
-	htmlTagLink: {
-		type: 'object',
-	},
 	contentWidth: {
 		type: 'string',
 		default: 'alignfull',
