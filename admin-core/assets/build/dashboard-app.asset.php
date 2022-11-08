a929c98769f0bed98a16613791316ea7d0b912f9
Zane Vaz <doofster@Zanes-MacBook-Air.local>
2022-11-08 05:48:58 +0530

    SPCTR-1916: Updated Popup and Cleaned Code

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-i18n'), 'version' => 'eca776537616f76cc904');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-i18n'), 'version' => 'ed29a6655ff8946d88e1');
