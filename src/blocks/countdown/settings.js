93c4481fbab1cc92a0ff1b2f6f620286bebea2c8
Antariksh Patre <antarikshpatre@gmail.com>
2022-11-18 12:44:02 +0530

    SPCTR-2057 Countdown- Basic settings setup

--- src/blocks/countdown/settings.js
+++ src/blocks/countdown/settings.js
@@ -1,3 +1,43 @@
+import UAGTextControl from '@Components/text-control';
+import UAGSelectControl from '@Components/select-control';
+import UAGAdvancedPanelBody from '@Components/advanced-panel-body';
+import InspectorTabs from '@Components/inspector-tabs/InspectorTabs.js';
+import InspectorTab, {
+	UAGTabs,
+} from '@Components/inspector-tabs/InspectorTab.js';
+import {
+    ToggleControl,
+	DateTimePicker,
+} from '@wordpress/components';
+import {
+    BlockControls,
+    InspectorControls
+} from '@wordpress/block-editor';
+
 export default function Settings( props ) {
-    return '';
+
+    props = props.parentProps;
+	const { setAttributes, attributes } = props;
+
+    const {
+        block_id,
+    } = attributes;
+
+    return (
+        <>
+            <InspectorControls>
+				<InspectorTabs>
+					<InspectorTab { ...UAGTabs.general }>
+					</InspectorTab>
+					<InspectorTab { ...UAGTabs.style }>
+					</InspectorTab>
+					<InspectorTab
+						{ ...UAGTabs.advance }
+						parentProps={ props }
+					>
+					</InspectorTab>
+				</InspectorTabs>
+			</InspectorControls>
+        </>
+    );
 };
