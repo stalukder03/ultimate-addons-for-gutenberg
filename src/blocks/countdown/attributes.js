98e516aa54a2c9271736cf48801ce8bee64791c5
Antariksh Patre <antarikshpatre@gmail.com>
2022-11-18 12:01:41 +0530

    SPCTR-2057 Countdown- Registered basic attributes

--- src/blocks/countdown/attributes.js
+++ src/blocks/countdown/attributes.js
@@ -12,6 +12,55 @@ const attributes = {
 		type: 'boolean',
 		default: false,
 	},
+	timerType: {
+		type: 'string',
+		default: 'date',  // date, evergreen, recurring.
+	},
+	dateTime: {
+		type: 'string',
+		default: '2030-01-01T00:00:00',
+	},
+	// Labels.
+	showLabels: {
+		type: 'boolean',
+		default: true,
+	},
+	labelDays: {
+		type: 'string',
+		default: __( 'DAYS', 'ultimate-addons-for-gutenberg' ),
+	},
+	labelHours: {
+		type: 'string',
+		default: __( 'HOURS', 'ultimate-addons-for-gutenberg' ),
+	},
+	labelMinutes: {
+		type: 'string',
+		default: __( 'MINUTES', 'ultimate-addons-for-gutenberg' ),
+	},
+	labelSeconds: {
+		type: 'string',
+		default: __( 'SECONDS', 'ultimate-addons-for-gutenberg' ),
+	},
+	// Separator.
+	showSeparator: {
+		type: 'boolean',
+		default: true,
+	},
+	separatorType: {
+		type: 'string',
+		default: 'colon',
+	},
+
+	// Countdown Expiry Settings.
+		// zero - keep at zero
+		// hide - hide timer
+		// redirect - redirect to url
+		// content - replace with innerblocks based content
+	timerEndAction: {
+		type: 'string',
+		default: 'zero',
+	}
+
 };
 
 export default attributes;
