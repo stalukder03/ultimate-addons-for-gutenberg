ba36cd49457c6000853c3262c4a3acb5c3b2c542
Antariksh Patre <antarikshpatre@gmail.com>
2022-11-21 11:45:06 +0530

    SPCTR-2057 Countdown- Updated save.js

--- src/blocks/countdown/save.js
+++ src/blocks/countdown/save.js
@@ -1,5 +1,33 @@
 import React from 'react';
+import classnames from 'classnames';
+
+import CountdownBox from './components/CountdownBox';
 
 export default function Save( props ) {
-    return null;
+
+	const { attributes } = props;
+
+    const {
+        block_id,
+        showLabels,
+        labelDays,
+        labelHours,
+        labelMinutes,
+        labelSeconds,
+    } = attributes;
+
+    return(
+        <div
+            className={ classnames(
+                props.className,
+                `uagb-block-${ block_id }`,
+                'wp-block-uagb-countdown',
+            ) }
+        >
+            <CountdownBox unitType='days' showLabels={ showLabels } label={ labelDays } />
+            <CountdownBox unitType='hours' showLabels={ showLabels } label={ labelHours } />
+            <CountdownBox unitType='minutes' showLabels={ showLabels } label={ labelMinutes } />
+            <CountdownBox unitType='seconds' showLabels={ showLabels } label={ labelSeconds } />
+        </div>
+    );
 };
