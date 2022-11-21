ca94847bd899f3375cc1e76151638c336fa067e7
Antariksh Patre <antarikshpatre@gmail.com>
2022-11-21 12:55:58 +0530

    SPCTR-2057 Countdown- Updated attributes.php with i18n

--- includes/blocks/countdown/attributes.php
+++ includes/blocks/countdown/attributes.php
@@ -10,9 +10,9 @@
 return array_merge(
 	array(
 		'showLabels'   => true,
-		'labelDays'    => 'DAYS',
-		'labelHours'   => 'HOURS',
-		'labelMinutes' => 'MINUTES',
-		'labelSeconds' => 'SECONDS',
+		'labelDays'    => __( 'DAYS', 'ultimate-addons-for-gutenberg' ),
+		'labelHours'   => __( 'HOURS', 'ultimate-addons-for-gutenberg' ),
+		'labelMinutes' => __( 'MINUTES', 'ultimate-addons-for-gutenberg' ),
+		'labelSeconds' => __( 'SECONDS', 'ultimate-addons-for-gutenberg' ),
 	)
 );
