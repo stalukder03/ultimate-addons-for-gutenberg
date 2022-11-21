c893155035ae002daf893ef571008bf3a3c09803
Antariksh Patre <antarikshpatre@gmail.com>
2022-11-21 16:01:28 +0530

    SPCTR-2057 Countdown- JS Logic- Completed basic updateCountdown function

--- assets/js/uagb-countdown.js
+++ assets/js/uagb-countdown.js
@@ -42,12 +42,28 @@ UAGBCountdown = { // eslint-disable-line no-undef
 
     updateCountdown( data ) {
 
+        // Wrappers.
+		const daysWrap = this.elements.countdownWrapper.querySelector( '.wp-block-uagb-countdown__time-days' );
+		const hoursWrap = this.elements.countdownWrapper.querySelector( '.wp-block-uagb-countdown__time-hours' );
+		const minutesWrap = this.elements.countdownWrapper.querySelector( '.wp-block-uagb-countdown__time-minutes' );
+		const secondsWrap = this.elements.countdownWrapper.querySelector( '.wp-block-uagb-countdown__time-seconds' );
+
+        // Calculations.
         const currentTime = new Date();
         const endDateTime = new Date( data.endDateTime );
-        
         const diff = endDateTime - currentTime;
+        const isOvertime = diff < 0;
 
-        const isOvertime = diff < 0
+        const days = Math.floor( diff / 1000 / 60 / 60 / 24 );
+        const hours = Math.floor( diff / 1000 / 60 / 60 ) % 24;
+        const minutes = Math.floor( diff / 1000 / 60 ) % 60;
+        const seconds = Math.floor( diff / 1000 ) % 60;
+
+        // Set the values.
+        daysWrap.innerHTML = ( ! isOvertime ) ? days : 0;
+        hoursWrap.innerHTML = ( ! isOvertime ) ? hours : 0;
+        minutesWrap.innerHTML = ( ! isOvertime ) ? minutes : 0;
+        secondsWrap.innerHTML = ( ! isOvertime ) ? seconds : 0;
 
     }
 
