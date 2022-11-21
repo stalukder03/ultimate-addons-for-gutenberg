9f59c53019054474b574216482ed318a78e65d49
Antariksh Patre <antarikshpatre@gmail.com>
2022-11-21 12:41:43 +0530

    SPCTR-2057 Countdown- Setup basic attributes.php

---
+++ includes/blocks/countdown/attributes.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Attributes File.
+ *
+ * @since 2.1.0
+ *
+ * @package uagb
+ */
+
+return array_merge(
+	array(
+		'showLabels'   => true,
+		'labelDays'    => 'DAYS',
+		'labelHours'   => 'HOURS',
+		'labelMinutes' => 'MINUTES',
+		'labelSeconds' => 'SECONDS',
+	)
+);
