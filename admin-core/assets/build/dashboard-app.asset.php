6327b53325a9f2fd33bd7d9efc5fcb2f8d17fc34
Antariksh Patre <antarikshpatre@gmail.com>
2022-12-08 15:17:53 +0530

    SPCTR-2057 Countdown - Admin Core - Fixed CSS color bug for disabled countdown icon

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'b209751442e32c21c464a20e90ace6c4');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'ffb511d2e85b7e24c0506c498bf0cc3a');
