d9f487a02e757f9f798680f9295cd336692e4062
Antariksh Patre <antarikshpatre@gmail.com>
2022-12-08 15:11:42 +0530

    SPCTR-2057 Countdown - Admin core build files

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'c5ac770202baee40724f');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'b209751442e32c21c464a20e90ace6c4');
