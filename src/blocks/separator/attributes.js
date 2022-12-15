fc6d09619f94d698d04cc45e67f3584210c48adb
Tushar Imran <tusharimran9909@gmail.com>
2022-12-15 20:54:17 +0600

    Separator Block - Added general and style settings

--- src/blocks/separator/attributes.js
+++ src/blocks/separator/attributes.js
@@ -9,12 +9,160 @@ const attributes = {
 		type: 'boolean',
 		default: false,
 	},
-	align: {
+	separatorAlign: {
 		type: 'string',
 		default: 'center',
 		UAGCopyPaste: {
-			styleType: 'column-align'
+			styleType: 'overall-alignment'
+		}
+	},
+	separatorAlignTablet: {
+		type: 'string',
+		default: '',
+		UAGCopyPaste: {
+			styleType: 'overall-alignment-tablet'
+		}
+	},
+	separatorAlignMobile: {
+		type: 'string',
+		default: '',
+		UAGCopyPaste: {
+			styleType: 'overall-alignment-mobile'
+		}
+	},
+	seperatorStyle: {
+		type: 'string',
+		default: 'none',
+		UAGCopyPaste: {
+			styleType: 'separator-style'
+		}
+	},
+	seperatorWidth: {
+		type: 'number',
+		UAGCopyPaste: {
+			styleType: 'separator-width'
 		},
+		default: 30,
+	},
+	seperatorWidthTablet: {
+		type: 'number',
+		UAGCopyPaste: {
+			styleType: 'separator-width'
+		},
+	},
+	seperatorWidthMobile: {
+		type: 'number',
+		UAGCopyPaste: {
+			styleType: 'separator-width'
+		},
+	},
+	separatorWidthType: {
+		type: 'string',
+		default: '%',
+		UAGCopyPaste: {
+			styleType: 'separator-width-type'
+		}
+	},
+	seperatorThickness: {
+		type: 'number',
+		default: 2,
+		UAGCopyPaste: {
+			styleType: 'separator-thickness'
+		}
+	},
+	thicknessUnit: {
+		type: 'string',
+		default: 'px',
+		UAGCopyPaste: {
+			styleType: 'separator-thickness-unit'
+		}
+	},
+	separatorTopPadding : {
+		type: 'number',
+		UAGCopyPaste: {
+			styleType: 'separator-top-margin'
+		}
+	},
+	separatorRightPadding : {
+		type: 'number',
+		UAGCopyPaste: {
+			styleType: 'separator-right-margin'
+		}
+	},
+	separatorLeftPadding : {
+		type: 'number',
+		UAGCopyPaste: {
+			styleType: 'separator-left-margin'
+		}
+	},
+	separatorPaddingTopTablet : {
+		type: 'number',
+		UAGCopyPaste: {
+			styleType: 'separator-top-margin-tablet'
+		}
+	},
+	separatorPaddingRightTablet : {
+		type: 'number',
+		UAGCopyPaste: {
+			styleType: 'separator-right-margin-tablet'
+		}
+	},
+	seperatorTabletSpace : {
+		type: 'number',
+		UAGCopyPaste: {
+			styleType: 'separator-bottom-margin-tablet'
+		}
+	},
+	separatorPaddingLeftTablet : {
+		type: 'number',
+		UAGCopyPaste: {
+			styleType: 'separator-left-margin-tablet'
+		}
+	},
+	separatorPaddingTopMobile : {
+		type: 'number',
+		UAGCopyPaste: {
+			styleType: 'separator-top-margin-mobile'
+		}
+	},
+	separatorPaddingRightMobile : {
+		type: 'number',
+		UAGCopyPaste: {
+			styleType: 'separator-right-margin-mobile'
+		}
+	},
+	seperatorMobileSpace : {
+		type: 'number',
+		UAGCopyPaste: {
+			styleType: 'separator-bottom-margin-mobile'
+		}
+	},
+	separatorPaddingLeftMobile : {
+		type: 'number',
+		UAGCopyPaste: {
+			styleType: 'separator-left-margin-mobile'
+		}
+	},
+	seperatorPaddingUnit : {
+		type: 'number',
+		default: 'px',
+		UAGCopyPaste: {
+			styleType: 'separator-margin-unit'
+		}
+	},
+	separatorMobilePaddingUnit : {
+		type: 'number',
+		UAGCopyPaste: {
+			styleType: 'separator-margin-unit-mobile'
+		},
+		default: 'px',
+	},
+	separatorTabletPaddingUnit : {
+		type: 'number',
+		UAGCopyPaste: {
+			styleType: 'separator-margin-unit-tablet'
+		},
+		default: 'px',
 	},
 };
 
--- src/blocks/separator/settings.js
+++ src/blocks/separator/settings.js
@@ -30,16 +30,328 @@ const Settings = ( props ) => {
 	const { attributes, deviceType, setAttributes } = props;
 	const {
 		block_id,
+		seperatorStyle,
+		separatorAlign,
+		separatorAlignTablet,
+		separatorAlignMobile,
+		seperatorWidth,
+		seperatorWidthTablet,
+		seperatorWidthMobile,
+		separatorWidthType,
+		seperatorColor,
+		seperatorThickness,
+		thicknessUnit,
+		separatorTopPadding,
+		separatorRightPadding,
+		seperatorBottomPadding,
+		separatorLeftPadding,
+		separatorPaddingTopTablet,
+		separatorPaddingRightTablet,
+		seperatorTabletPadding,
+		separatorPaddingLeftTablet,
+		separatorPaddingTopMobile,
+		separatorPaddingRightMobile,
+		seperatorMobilePadding,
+		separatorPaddingLeftMobile,
+		seperatorBottomPaddingUnit,
+		separatorMobilePaddingUnit,
+		separatorTabletPaddingUnit,
+		separatorPaddingLink,
 	} = attributes;
 
+	// Separator settings.
+	const seperatorGeneralSettings = () => {
+		return (
+			<UAGAdvancedPanelBody
+				title={ __( 'Separator', 'ultimate-addons-for-gutenberg' ) }
+				initialOpen={ true }
+			>
+				<UAGSelectControl
+					label={ __(
+						'Style',
+						'ultimate-addons-for-gutenberg'
+					) }
+					data={ {
+						value: seperatorStyle,
+						label: 'seperatorStyle',
+					} }
+					setAttributes={ setAttributes }
+					options={ [
+						{
+							value: 'none',
+							label: __(
+								'None',
+								'ultimate-addons-for-gutenberg'
+							),
+						},
+						{
+							value: 'solid',
+							label: __(
+								'Solid',
+								'ultimate-addons-for-gutenberg'
+							),
+						},
+						{
+							value: 'double',
+							label: __(
+								'Double',
+								'ultimate-addons-for-gutenberg'
+							),
+						},
+						{
+							value: 'dashed',
+							label: __(
+								'Dashed',
+								'ultimate-addons-for-gutenberg'
+							),
+						},
+						{
+							value: 'dotted',
+							label: __(
+								'Dotted',
+								'ultimate-addons-for-gutenberg'
+							),
+						},
+					] }
+				/>
+				<MultiButtonsControl
+					setAttributes={ setAttributes }
+					label={ __(
+						'Alignment',
+						'ultimate-addons-for-gutenberg'
+					) }
+					responsive={true}
+					data={ {
+						desktop: {
+							value: separatorAlign,
+							label: 'separatorAlign',
+						},
+						tablet: {
+							value: separatorAlignTablet,
+							label: 'separatorAlignTablet',
+						},
+						mobile: {
+							value: separatorAlignMobile,
+							label: 'separatorAlignMobile',
+						},
+					} }
+					className="uagb-multi-button-alignment-control"
+					options={ [
+						{
+							value: 'left',
+							icon: (
+								<Icon
+									icon={ renderSVG( 'fa fa-align-left' ) }
+								/>
+							),
+							tooltip: __(
+								'Left',
+								'ultimate-addons-for-gutenberg'
+							),
+						},
+						{
+							value: 'center',
+							icon: (
+								<Icon
+									icon={ renderSVG(
+										'fa fa-align-center'
+									) }
+								/>
+							),
+							tooltip: __(
+								'Center',
+								'ultimate-addons-for-gutenberg'
+							),
+						},
+						{
+							value: 'right',
+							icon: (
+								<Icon
+									icon={ renderSVG(
+										'fa fa-align-right'
+									) }
+								/>
+							),
+							tooltip: __(
+								'Right',
+								'ultimate-addons-for-gutenberg'
+							),
+						},
+					] }
+					showIcons={ true }
+				/>
+			</UAGAdvancedPanelBody>
+		);
+	};
+
+	const seperatorStyleSettings = () => {
+		return (
+			<UAGAdvancedPanelBody
+				title="Separator"
+				initialOpen={ true }
+			>
+				<ResponsiveSlider
+					label={ __(
+						'Width',
+						'ultimate-addons-for-gutenberg'
+					) }
+					data={ {
+						desktop: {
+							value: seperatorWidth,
+							label: 'seperatorWidth',
+						},
+						tablet: {
+							value: seperatorWidthTablet,
+							label: 'seperatorWidthTablet',
+						},
+						mobile: {
+							value: seperatorWidthMobile,
+							label: 'seperatorWidthMobile',
+						},
+					} }
+					min={ 0 }
+					max={ '%' === separatorWidthType ? 100 : 500 }
+					unit={ {
+						value: separatorWidthType,
+						label: 'separatorWidthType',
+					} }
+					units={ [
+						{
+							name: __(
+								'Pixel',
+								'ultimate-addons-for-gutenberg'
+							),
+							unitValue: 'px',
+						},
+						{
+							name: __(
+								'%',
+								'ultimate-addons-for-gutenberg'
+							),
+							unitValue: '%',
+						},
+					] }
+					setAttributes={ setAttributes }
+				/>
+				<Range
+					label={ __(
+						'Thickness',
+						'ultimate-addons-for-gutenberg'
+					) }
+					setAttributes={ setAttributes }
+					value={ seperatorThickness }
+					data={ {
+						value: seperatorThickness,
+						label: 'seperatorThickness',
+					} }
+					min={ 0 }
+					max={ 10 }
+					unit={ {
+						value: thicknessUnit,
+						label: 'thicknessUnit',
+					} }
+				/>
+				<AdvancedPopColorControl
+					label={ __(
+						'Color',
+						'ultimate-addons-for-gutenberg'
+					) }
+					colorValue={
+						seperatorColor ? seperatorColor : ''
+					}
+					data={ {
+						value: seperatorColor,
+						label: 'seperatorColor',
+					} }
+					setAttributes={ setAttributes }
+				/>
+				<SpacingControl
+					{ ...props }
+					label={ __(
+						'Padding',
+						'ultimate-addons-for-gutenberg'
+					) }
+					valueTop={ {
+						value: separatorTopPadding,
+						label: 'separatorTopPadding',
+					} }
+					valueRight={ {
+						value: separatorRightPadding,
+						label: 'separatorRightPadding',
+					} }
+					valueBottom={ {
+						value: seperatorBottomPadding,
+						label: 'seperatorBottomPadding',
+					} }
+					valueLeft={ {
+						value: separatorLeftPadding,
+						label: 'separatorLeftPadding',
+					} }
+					valueTopTablet={ {
+						value: separatorPaddingTopTablet,
+						label: 'separatorPaddingTopTablet',
+					} }
+					valueRightTablet={ {
+						value: separatorPaddingRightTablet,
+						label: 'separatorPaddingRightTablet',
+					} }
+					valueBottomTablet={ {
+						value: seperatorTabletPadding,
+						label: 'seperatorTabletPadding',
+					} }
+					valueLeftTablet={ {
+						value: separatorPaddingLeftTablet,
+						label: 'separatorPaddingLeftTablet',
+					} }
+					valueTopMobile={ {
+						value: separatorPaddingTopMobile,
+						label: 'separatorPaddingTopMobile',
+					} }
+					valueRightMobile={ {
+						value: separatorPaddingRightMobile,
+						label: 'separatorPaddingRightMobile',
+					} }
+					valueBottomMobile={ {
+						value: seperatorMobilePadding,
+						label: 'seperatorMobilePadding',
+					} }
+					valueLeftMobile={ {
+						value: separatorPaddingLeftMobile,
+						label: 'separatorPaddingLeftMobile',
+					} }
+					unit={ {
+						value: seperatorBottomPaddingUnit,
+						label: 'seperatorBottomPaddingUnit',
+					} }
+					mUnit={ {
+						value: separatorMobilePaddingUnit,
+						label: 'separatorMobilePaddingUnit',
+					} }
+					tUnit={ {
+						value: separatorTabletPaddingUnit,
+						label: 'separatorTabletPaddingUnit',
+					} }
+					deviceType={ deviceType }
+					attributes={ attributes }
+					setAttributes={ setAttributes }
+					link={ {
+						value: separatorPaddingLink,
+						label: 'separatorPaddingLink',
+					} }
+				/>
+			</UAGAdvancedPanelBody>
+		);
+	}
 
 	return (
 		<div>
 			<InspectorControls>
 				<InspectorTabs>
 					<InspectorTab { ...UAGTabs.general }>
+						{seperatorGeneralSettings()}
 					</InspectorTab>
 					<InspectorTab { ...UAGTabs.style }>
+						{seperatorStyleSettings()}
 					</InspectorTab>
 					<InspectorTab
 						{ ...UAGTabs.advance }
